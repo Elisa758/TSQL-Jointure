8f5a3e96e3ac1f81cd13c3aac04bee887bca510c
Elisa758 <57760897+Elisa758@users.noreply.github.com>
2020-02-27 09:17:22 +0100

    Update CreationDB_Relation.sql

--- CreationDB_Relation.sql
+++ CreationDB_Relation.sql
@@ -1,5 +1,5 @@
---CREATE DATABASE Relation;
-USE Relation;
+--CREATE DATABASE MartienRelation;
+USE MartienRelation;
 
 CREATE TABLE Continent(
 idContinent INT PRIMARY KEY IDENTITY(1,1),
@@ -9,11 +9,6 @@ nameContinent VARCHAR(50)
 CREATE TABLE Base(
 idBase INT PRIMARY KEY IDENTITY(1,1),
 nameBase VARCHAR(50)
-);
-
-CREATE TABLE MartienSuperieur(
-idSuperieur INT PRIMARY KEY IDENTITY(1,1),
-nameSuperieur VARCHAR(20)
 );
 
 CREATE TABLE Terrien(
@@ -35,7 +30,7 @@ REFERENCES Terrien(idTerrien),
 CONSTRAINT fk_baseRef FOREIGN KEY (base_id) 
 REFERENCES Base(idBase),
 CONSTRAINT fk_supRef FOREIGN KEY (superieur_id) 
-REFERENCES MartienSuperieur(idSuperieur)
+REFERENCES Martien(idMartien)
 );
 
 INSERT INTO Continent (nameContinent)
@@ -48,30 +43,24 @@ VALUES ('Cratere'),
 ('New Town'),
 ('Olympus');
 
-INSERT INTO MartienSuperieur(nameSuperieur)
-VALUES ('Caporal'),
-('Empereur'),
-('Lieutenant');
-
 INSERT INTO Terrien(nameTerrien,continent_id)
 VALUES ('Hans', 3),
 ('Kristoff', 1),
 ('Ursula',1);
 
 SELECT * FROM Base;
-SELECT * FROM MartienSuperieur;
 SELECT * FROM Terrien;
 SELECT * FROM Continent;
 
 INSERT INTO Martien(nameMartien, superieur_id, terrien_id, base_id)
-VALUES('Empereur',2, 3, 3),
+VALUES('Empereur',1, 3, 3),
 ('Kahl',1,2,3),
 ('Nyx', 2,1,1);
 
 SELECT * FROM Martien;
 
-SELECT m.nameMartien,t.nameTerrien, m.base_id, b.nameBase, t.continent_id, c.nameContinent
+SELECT m.nameMartien, m.superieur_id,t.nameTerrien, m.base_id, b.nameBase, t.continent_id, c.nameContinent
 FROM Martien AS m
-FULL OUTER JOIN Terrien AS t ON m.terrien_id =t.idTerrien
-FULL OUTER JOIN Base AS b ON m.base_id=b.idBase
-FULL OUTER JOIN Continent AS c ON t.continent_id=c.idContinent;
+INNER JOIN Terrien AS t ON m.terrien_id =t.idTerrien
+INNER JOIN Base AS b ON m.base_id=b.idBase
+INNER JOIN Continent AS c ON t.continent_id=c.idContinent;
